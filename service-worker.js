815b772f174992324e9c11c42408a69d223f41b5
Eissa89 <111297573+Eissa89@users.noreply.github.com>
2025-07-30 09:16:23 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,14 +1,19 @@
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open('red-stillness-cache').then(cache => {
-      return cache.addAll(['/', 'index.html']);
+    caches.open("red-stillness-cache").then((cache) => {
+      return cache.addAll([
+        "/THE-RED-STILLNESS/",
+        "/THE-RED-STILLNESS/index.html",
+        "/THE-RED-STILLNESS/icon-192.png",
+        "/THE-RED-STILLNESS/icon-512.png"
+      ]);
     })
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => {
+    caches.match(event.request).then((response) => {
       return response || fetch(event.request);
     })
   );
